d6522f3fd80a974eaf7c214c963e16ca03359905
Humma Noshine <Hummanoshine@gmail.com>
2022-02-01 19:23:44 +0000

    trying some apis on js

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-function recipe() {
-  var url = `https://recipesapi2.p.rapidapi.com/recipes/lasagna`;
-
-  fetch(url).then(function (response) {
-    if (response.ok) {
-        response.json().then(function (data) {
-
-            
-  });
-}
-
-var options = {
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://recipesapi2.p.rapidapi.com/recipes/tomato%20soup?maxRecipes=2",
   method: "GET",
-  url: "https://recipesapi2.p.rapidapi.com/recipes/lasagna",
-  params: { maxRecipes: "2" },
   headers: {
     "x-rapidapi-host": "recipesapi2.p.rapidapi.com",
     "x-rapidapi-key": "99487931c9msh09e061c599bd40dp1e25e6jsn37d27e63886d",
   },
 };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
